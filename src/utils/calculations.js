547a97dfd3bf46b1b0ebef446fa9872e6d8ff30b
vignesh_c <vignesh_c@solartis.com>
2024-11-19 12:00:14 +0530

    Refactored components into separate files and fixed Row import issue

--- src/utils/calculations.js
+++
@@ -1,7 +0,0 @@
-export const calculateSubtotal = (children) => {
-    return children.reduce((total, child) => total + child.value, 0);
-};
-
-export const calculateVariance = (original, updated) => {
-    return ((updated - original) / original) * 100;
-};
